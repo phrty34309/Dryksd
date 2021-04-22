35bacb64fb2be62bcb63c6d1ee91053fb0915f24
phrty34309 <ulya.shelep@bk.ru>
2021-04-22 09:14:23 +0800

    Сделал Appbar в sight_list_screen.dart с Text

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -10,9 +10,15 @@ class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      backgroundColor: Colors.yellowAccent,
+      backgroundColor: Colors.white,
       appBar: new AppBar(
-        title: new Text("My Places"),
+        toolbarHeight: 120, //  высоту
+        elevation: 0.0, //убрал тень
+        title: new Text("Список\nинтересных мест",
+        textAlign: TextAlign.left,
+        style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.white,
       ),
 
     );
