ddc6b4a894e177e6435f789e732a43bd75e5ac4b
phrty34309 <ulya.shelep@bk.ru>
2021-04-23 18:00:03 +0800

    Сделал Appbar в sight_list_screen.dart с RichText и увеличил шрифт и его же сделал пожирнее. Отформатировал код.

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class SightListScreen extends StatefulWidget {
   @override
   _SightListScreenState createState() => _SightListScreenState();
@@ -10,29 +9,51 @@ class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      backgroundColor: Colors.white,
-      appBar: new AppBar(
-        toolbarHeight: 120, //  высоту
-        elevation: 0.0, //убрал тень
-        title: RichText(
-          text: TextSpan(text: 'С', style: TextStyle(color: Colors.amber),
-              children: [TextSpan(text: 'писок \n', style: TextStyle(color: Colors.black),
-            children:[TextSpan(text: 'И', style: TextStyle(color: Colors.yellowAccent),
-            children: [TextSpan(text: 'нтересных', style: TextStyle(color: Colors.black),
-                children:[TextSpan(text: ' мест', style: TextStyle(color: Colors.black),
-                )
-                ]
-            )
-            ]
-            )
-            ]
-              )
-              ]
-
-        )
-      ),
-       backgroundColor: Colors.white,
-    )
-    );
+        backgroundColor: Colors.white,
+        appBar: new AppBar(
+          toolbarHeight: 120, //  высоту
+          elevation: 0.0, //убрал тень
+          title: RichText(
+              text: TextSpan(
+                  text: 'С',
+                  style: TextStyle(
+                      color: Colors.amber, //amber
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold),
+                  children: [
+                TextSpan(
+                    text: 'писок \n',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold),
+                    children: [
+                      TextSpan(
+                          text: 'И',
+                          style: TextStyle(
+                              color: Colors.yellowAccent, //yellowAccent
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold),
+                          children: [
+                            TextSpan(
+                                text: 'нтересных',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 32,
+                                    fontWeight: FontWeight.bold),
+                                children: [
+                                  TextSpan(
+                                    text: ' мест',
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 32,
+                                        fontWeight: FontWeight.bold),
+                                  )
+                                ])
+                          ])
+                    ])
+              ])),
+          backgroundColor: Colors.white,
+        ));
   }
 }
