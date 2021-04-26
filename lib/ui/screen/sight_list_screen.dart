3392c67b58db0b3e426144e966c79968dae298ff
phrty34309 <ulya.shelep@bk.ru>
2021-04-26 18:12:23 +0800

    увеличил шрифт, сделал пожирнее, убрал new

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -8,51 +8,17 @@ class SightListScreen extends StatefulWidget {
 class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
+    return Scaffold(
         backgroundColor: Colors.white,
-        appBar: new AppBar(
+        appBar:  AppBar(
+          title:  Text(
+            "Список\nинтересных мест",
+            textAlign: TextAlign.left,
+            style: TextStyle(
+                color: Colors.black, fontSize: 32, fontWeight: FontWeight.bold),
+          ),
           toolbarHeight: 120, //  высоту
           elevation: 0.0, //убрал тень
-          title: RichText(
-              text: TextSpan(
-                  text: 'С',
-                  style: TextStyle(
-                      color: Colors.amber, //amber
-                      fontSize: 32,
-                      fontWeight: FontWeight.bold),
-                  children: [
-                TextSpan(
-                    text: 'писок \n',
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold),
-                    children: [
-                      TextSpan(
-                          text: 'И',
-                          style: TextStyle(
-                              color: Colors.yellowAccent, //yellowAccent
-                              fontSize: 32,
-                              fontWeight: FontWeight.bold),
-                          children: [
-                            TextSpan(
-                                text: 'нтересных',
-                                style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 32,
-                                    fontWeight: FontWeight.bold),
-                                children: [
-                                  TextSpan(
-                                    text: ' мест',
-                                    style: TextStyle(
-                                        color: Colors.black,
-                                        fontSize: 32,
-                                        fontWeight: FontWeight.bold),
-                                  )
-                                ])
-                          ])
-                    ])
-              ])),
           backgroundColor: Colors.white,
         ));
   }
