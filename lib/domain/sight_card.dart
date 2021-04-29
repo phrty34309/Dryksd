99b902a8c7b06816002a8d34eb822bf5e85a9f85
phrty34309 <ulya.shelep@bk.ru>
2021-04-29 23:32:33 +0800

    создал в папке lib папку domain.
    В папке domain создал файл sight.dart. Файл содержит модель данных Sight с полями
    
    В папке lib создал файл mocks.dart. Файл содержит коллекцию моковых достопримечательностей final List mocks = [];
    
    Инициализировал несколько интересных мест

---
+++ lib/domain/sight_card.dart
@@ -0,0 +1,9 @@
+class Sight {
+  final String name;
+  final String geoPosition;
+  final String url;
+  final String details;
+  final String type;
+
+  Sight(this.name, this.geoPosition, this.url, this.details, this.type);
+}
